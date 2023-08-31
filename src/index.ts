a8fd5a7b4303deb9c288978bb2ddd627d1859f45
redaphid <iam@aaronherres.com>
2023-08-31 15:46:18 -0700

    finally, something that works

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,19 @@
+import {readFileSync} from 'fs';
+import {gql} from 'graphql-tag';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { buildSubgraphSchema } from "@apollo/subgraph";
-import gql from "graphql-tag";
 import Resolvers from './resolvers';
-import {readFileSync} from 'fs';
 
 const main = async () => {
+  const typeDefs = buildSubgraphSchema(gql(readFileSync('./schema.graphql', 'utf-8')))
+
   const server = new ApolloServer({
     resolvers: Resolvers,
-    typeDefs: buildSubgraphSchema(gql(readFileSync('./schema.graphql', 'utf-8'))),
+    typeDefs,
   });
 
-  const { url } = await startStandaloneServer(server, {
-    // Note: This example uses the `req` argument to access headers,
-    // but the arguments received by `context` vary by integration.
-    // This means they vary for Express, Fastify, Lambda, etc.
-
-    // For `startStandaloneServer`, the `req` and `res` objects are
-    // `http.IncomingMessage` and `http.ServerResponse` types.
-    context: async ({ req, res }) => {
-      // Get the user token from the headers.
-      const token = req.headers.authorization || '';
-
-      // Try to retrieve a user with the token
-      // const user = await getUser(token);
-
-      // Add the user to the context
-      return { user: 'whatever' };
-    },
-  });
+  const { url } = await startStandaloneServer(server, {});
   console.log(`🚀 Server listening at: ${url}`);
 }
 main();
