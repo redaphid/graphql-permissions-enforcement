0f69d4a935fa48ad212c7225a767fdcb4b16ff3c
redaphid <iam@aaronherres.com>
2023-09-01 15:21:16 -0700

    filter moar

--- src/directives/public.ts
+++ src/directives/public.ts
@@ -1,43 +1,88 @@
-import { filterSchema } from '@graphql-tools/utils';
-import { GraphQLSchema, isInputObjectType, isObjectType, printSchema } from 'graphql';
+import { ArgumentFilter, FieldFilter, ObjectFieldFilter, TypeFilter, filterSchema, getDirectives } from '@graphql-tools/utils';
+import { GraphQLSchema, isEnumType, isInputObjectType, isInterfaceType, isListType, isObjectType, isScalarType, isUnionType, printSchema } from 'graphql';
 
-const removeEmptyRootTypes = (schema) => {
+const isEmptyRootType = (rootType, schema) => {
+  if (!rootType) return true;
+  const directives = getDirectives(schema, rootType);
+  if (directives && directives.some(d => d.name === 'public')) {
+    return false;
+  }
+  return true
+}
+const removeEmptyRootTypes = (schema: GraphQLSchema) => {
   const schemaConfig = schema.toConfig();
-  const newTypes = schemaConfig.types.filter(type => {
-    if (isObjectType(type) || isInputObjectType(type)) {
-      return Object.keys(type.getFields()).length > 0;
-    }
-    return true;
-  });
+  let query = schemaConfig.query;
+  if (isEmptyRootType(query, schema)) {
+    schemaConfig.query = null;
+  }
+
+  let mutation = schemaConfig.mutation;
+  if (isEmptyRootType(mutation, schema)) {
+    schemaConfig.mutation = null;
+  }
 
   return new GraphQLSchema({
     ...schemaConfig,
-    types: newTypes,
-    query: Object.keys(schemaConfig.query.getFields()).length > 0 ? schemaConfig.query : null,
-    mutation: Object.keys(schemaConfig.mutation.getFields()).length > 0 ? schemaConfig.mutation : null,
   });
-};
+}
+const removeNonPublicTypes = (schema: GraphQLSchema) => {
 
-const removeNonPublicTypes = (schema) => {
+  const hasPublicDirective: FieldFilter = (typeName, fieldName, fieldConfig) => {
+    if (typeName.startsWith('_') || typeName.includes('__')) return true;
+    return Boolean(fieldConfig.astNode?.directives?.some(d => d.name.value === 'public'));
+  }
+
+  const typeFilterNonPublic: TypeFilter = (typeName, type) => {
+    if (isObjectType(type) || isInputObjectType(type) || isInterfaceType(type)) {
+      if (Object.keys(type.getFields()).length === 0) {
+        return false;
+      }
+      if (!getDirectives(schema, type)?.some(d => d.name === 'public')) return false;
+      return true;
+    }
+
+    if (isListType(type)) {
+      return typeFilterNonPublic(typeName, type.ofType);
+    }
+    if (isUnionType(type)) {
+      return type.getTypes().every(t => typeFilterNonPublic(typeName, t));
+    }
+    if (isEnumType(type)) {
+      return true
+    }
+    if (isScalarType(type)) {
+      return true
+    }
+    return true
+  }
+
+  const filterNonPublicFields: FieldFilter = (typeName, fieldName, fieldConfig) => {
+    if (fieldName.startsWith('_')) return true;
+    if (!hasPublicDirective(typeName, fieldName, fieldConfig)) return false;
+    if (!typeFilterNonPublic(typeName, fieldConfig.type)) return false;
+    if (isInputObjectType(fieldConfig.type)) {
+      if (Object.keys(fieldConfig.type.getFields()).length === 0) return false;
+    }
+    // throw new Error(`I would remove this field: ${typeName}, ${fieldName}`)
+    return true
+  }
+
+  const filterNonPublicArguments: ArgumentFilter = (typeName, fieldName, argNam, argConfig?) => {
+    if (!argConfig) return false;
+    if (!hasPublicDirective(typeName || '', fieldName || '', argConfig)) return false;
+    if (!typeFilterNonPublic(typeName || '', argConfig.type)) return false;
+    return true
+  }
+
   return filterSchema({
     schema,
-    fieldFilter: (typeName, fieldName, fieldConfig) => {
-      return Boolean(fieldConfig.astNode?.directives?.some(d => d.name.value === 'public'));
-    },
-    rootFieldFilter: (operationType, fieldName) => {
-      const rootType = schema.getType(operationType);
-      if (isObjectType(rootType)) {
-        return Object.keys(rootType.getFields()).length > 0;
-      }
-      return false;
-    },
-    typeFilter: (typeName, type) => {
-      if (isObjectType(type) || isInputObjectType(type)) {
-        // Include the type only if it has one or more fields
-        return Object.keys(type.getFields()).length > 0;
-      }
-      return true;  // keep other types
-    },
+    rootFieldFilter: filterNonPublicFields,
+    typeFilter: typeFilterNonPublic,
+    fieldFilter: filterNonPublicFields,
+    objectFieldFilter: filterNonPublicFields,
+    interfaceFieldFilter: filterNonPublicFields,
+    inputObjectFieldFilter: filterNonPublicFields,
+    // argumentFilter: filterNonPublicArguments,
   });
 }
 
@@ -51,5 +96,7 @@ export const PublicDirectiveTransformer = (schema: GraphQLSchema) => {
     filteredSchema = removeEmptyRootTypes(removeNonPublicTypes(filteredSchema));
     times++
   } while (printSchema(filteredSchema) !== lastSchemaString);
+  console.log(printSchema(filteredSchema));
   return filteredSchema;
 };
+
